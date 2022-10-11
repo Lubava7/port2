6ec2e703e9deec9b8e02c19cc49b46cf4f4a9144
Lubava7 <joheunsaram@mail.ru>
2022-10-11 20:12:56 +0300

    nothing to commit

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import project from "../../styles/Projects.module.scss";
+// import project from "../../styles/Projects.module.scss";
 
 function Projects() {
   return <div>Projects</div>;
